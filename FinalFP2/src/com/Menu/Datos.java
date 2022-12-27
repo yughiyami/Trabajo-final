6d44423dc8abda588339235f1456541c03373fea
yughiyami <116712492+yughiyami@users.noreply.github.com>
2022-12-27 07:42:10 -0500

    lee

---
+++ FinalFP2/src/com/Menu/Datos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Menu;
+
+/**
+ *
+ * @author PC
+ */
+import java.awt.Color;
+interface Datos {
+	
+    String[] modos = {"Clásico","Turbo"};
+
+    String[] consejos = {""};
+	
+    String[] ayudas =   {"Gana el que consiga mejores pistas al final de la partida",
+			"Similar al clásico pero solo tienen 1 día"};
+}
